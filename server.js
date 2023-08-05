a9c5dba43f61dae693a63dbd49fa9b3a84da6761
kerBamf <zach.pedigo@gmail.com>
2023-08-05 16:01:48 -0400

    Fixed string typo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 
 app.get('/', (req, res) => {
-    res.send(<h1>Hello all ye nations of planet EARF</h1>)
+    res.send("<h1>Hello all ye nations of planet EARF</h1>")
 })
 
 
